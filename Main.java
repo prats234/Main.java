7ffe1ab197e652180611682d21b0e47f4415ce9f
prats234 <109065276+prats234@users.noreply.github.com>
2022-07-11 20:04:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 30;
+        short b = 20;
+        double c = 40.4;
+        char d = 't';
+
+        {
+            System.out.println("addition of a & c is");
+            System.out.println(a+c);
+        }
+    }
+}
